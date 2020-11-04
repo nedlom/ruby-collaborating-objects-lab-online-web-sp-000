7128b1d876ae566a6a4335cdc58c8bbf0cc32040
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-04 18:45:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   def self.new_by_filename(file_name)
     song = self.new(file_name.split(" - ")[1])
     song.artist_name = file_name.split(" - ")[0]
-    song
+    #song
   end
   
   def artist_name=(name)
